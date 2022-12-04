b2cfbab93dbb8cc5ca8b8fc7a84db9ce9322a318
Егор Ковешников <egor.KW@yandex.ru>
2022-12-04 19:50:51 +0300

    add validation

--- ReadyList.Services/Models/User.cs
+++ ReadyList.Services/Models/User.cs
@@ -1,12 +1,26 @@
-﻿namespace ReadyList.Database.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReadyList.Database.Models
 {
     public class User
     {
         public Guid UserId { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+        [MaxLength(25)]
+        public string? FirstName { get; set; }
+
+        [MaxLength(25)]
+        public string? MiddleName { get; set; }
+
+        public string? LastName { get; set; }
     }
 }
